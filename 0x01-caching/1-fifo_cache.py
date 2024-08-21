1b2f5bade3c34275ac04d69f3f3d789846a1c36c
rekebuba <abukiabdullahi1@gmail.com>
2024-08-21 19:48:37 +0300

    FIFO caching Algorithm

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,43 @@
 #!/usr/bin/env python3
+"""FIFO caching Algorithm"""
+from typing import Any, Optional
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """base class"""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def put(self, key: Optional[Any], item: Optional[Any]) -> None:
+        """Must assign to the dictionary the item value for the key.
+        If the number of items in is higher that BaseCaching.MAX_ITEMS
+        implement FIFO Algorithm
+
+        Args:
+            key (Optional[Any])
+            item (Optional[Any])
+        """
+        if key is not None and item is not None:
+            valid = (
+                len(self.cache_data) >= BaseCaching.MAX_ITEMS and
+                key not in self.cache_data
+            )
+            if valid:
+                first_key = list(self.cache_data)[0][0]
+                print(f'DISCARD: {first_key}')
+                del self.cache_data[first_key]
+
+            self.cache_data[key] = item
+
+    def get(self, key: Optional[Any]) -> Optional[Any]:
+        """Must return the value linked to key.
+
+        Args:
+            key (Optional[Any])
+
+        Returns:
+            Optional[Any]: return item in dictionary
+        """
+        return self.cache_data.get(key, None)
