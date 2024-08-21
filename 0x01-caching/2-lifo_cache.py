ac4a160f66082cec263cffb24c887bb30182d02a
rekebuba <abukiabdullahi1@gmail.com>
2024-08-21 22:19:22 +0300

    LIFO caching Algorithm

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+"""LIFO caching Algorithm"""
+from typing import Any, Optional
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """base class"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key: Optional[Any], item: Optional[Any]) -> None:
+        """Must assign to the dictionary the item value for the key.
+        If the number of items in is higher that BaseCaching.MAX_ITEMS
+        implement LIFO Algorithm
+
+        Args:
+            key (Optional[Any])
+            item (Optional[Any])
+        """
+        if key is not None and item is not None:
+            valid = (
+                len(self.cache_data) >= BaseCaching.MAX_ITEMS and
+                key not in self.cache_data
+            )
+            if valid:
+                first_key, _ = self.cache_data.popitem()
+                print("DISCARD:", first_key)
+
+            self.cache_data[key] = item
+
+    def get(self, key: Optional[Any]) -> Optional[Any]:
+        """Must return the value linked to key.
+
+        Args:
+            key (Optional[Any])
+
+        Returns:
+            Optional[Any]: return item in dictionary
+        """
+        return self.cache_data.get(key, None)
